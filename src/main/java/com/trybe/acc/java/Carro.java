c7c959bafa01662dec77966aa74c77b78ab11f8c
eon <bernardomp.dev@gmail.com>
2023-08-23 17:22:12 -0300

    Feat: Carro Class.

--- src/main/java/com/trybe/acc/java/Carro.java
+++ src/main/java/com/trybe/acc/java/Carro.java
@@ -6,8 +6,33 @@ package com.trybe.acc.java;
 
 public class Carro extends VeiculoBase {
 
-  /**
-   * Atributos e Métodos.
-   */
+  private final int numeroDePassageiros;
+
+  public Carro(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      int numeroDePassageiros
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.numeroDePassageiros = numeroDePassageiros;
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    return distancia / 10;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + super.getMarca());
+    System.out.println("Modelo: " + super.getModelo());
+    System.out.println("Ano: " + super.getAno());
+    System.out.println("Tipo de combustivel: " + super.getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + super.getCapacidadeTanque());
+    System.out.println("Número de Passageiros: " + this.numeroDePassageiros);
+  }
 
 }
