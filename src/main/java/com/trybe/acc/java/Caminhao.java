579e522299ad7c75e4c6e9509d8df70509fb0f57
eon <bernardomp.dev@gmail.com>
2023-08-23 17:22:50 -0300

    Feat: Caminhao Class.

--- src/main/java/com/trybe/acc/java/Caminhao.java
+++ src/main/java/com/trybe/acc/java/Caminhao.java
@@ -10,4 +10,32 @@ public class Caminhao extends VeiculoBase {
    * Atributos e Métodos.
    */
 
+  private final int numeroDePassageiros;
+
+  public Caminhao(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      int numeroDePassageiros
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.numeroDePassageiros = numeroDePassageiros;
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    return distancia / 6.0;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + super.getMarca());
+    System.out.println("Modelo: " + super.getModelo());
+    System.out.println("Ano: " + super.getAno());
+    System.out.println("Tipo de combustivel: " + super.getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + super.getCapacidadeTanque());
+    System.out.println("Número de Passageiros: " + this.numeroDePassageiros);
+  }
 }
