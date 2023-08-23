06656e8290a4d0ae65d6ca4de3d44d80a16367b0
eon <bernardomp.dev@gmail.com>
2023-08-23 17:22:57 -0300

    Feat: Moto Class.

--- src/main/java/com/trybe/acc/java/Moto.java
+++ src/main/java/com/trybe/acc/java/Moto.java
@@ -10,4 +10,31 @@ public class Moto extends VeiculoBase {
    * Atributos e Métodos.
    */
 
+  private String tipo;
+
+  public Moto(
+      String marca,
+      String modelo,
+      int ano,
+      String tipoCombustivel,
+      int capacidadeTanque,
+      String tipo
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    return distancia / 18.0;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + super.getMarca());
+    System.out.println("Modelo: " + super.getModelo());
+    System.out.println("Ano: " + super.getAno());
+    System.out.println("Tipo de combustivel: " + super.getTipoCombustivel());
+    System.out.println("Capacidade do Tanque: " + super.getCapacidadeTanque());
+  }
+
 }
